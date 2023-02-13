31f65a5c1a1de4cd555d88a5cfb41f48c1cc1d60
Shannon <shogeboom187@gmail.com>
2023-02-12 20:43:26 -0700

    <your message here>

---
+++ assets/script.js
@@ -0,0 +1,21 @@
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+for (var x = 0; x < topics.length; x++) {
+ console.log(topics[x]);
+}
+
+/*if(topics === 'HTML'){
+    console.log("Lets study HTML!");
+}
+else if (topics === 'CSS'){
+    console.log("Lets study CSS!");
+}
+else if(topics === 'Git'){
+    console.log("Lets study Git!");
+}
+else if(topics === 'JavaScript'){
+    console.log("Lets study JavaScript!");
+}
+else{
+    console.log('Please try again!');
+}*/
